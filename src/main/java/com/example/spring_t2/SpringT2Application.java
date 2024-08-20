3bf965633104e5f3c49b7e69124ebcfdfd9cf3e2
praveenkotipalli <praveenin321@gmail.com>
2024-08-20 12:21:19 +0530

    first commit

---
+++ src/main/java/com/example/spring_t2/SpringT2Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_t2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringT2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringT2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_t2/SpringT2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_t2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringT2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
